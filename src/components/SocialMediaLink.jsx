e32a44778a2b1f757f072a804dc5d6b65ee982dd
Ezekiel <zekejenyart@gmail.com>
2020-09-10 15:16:50 -0500

    flush and restructure of files

--- src/components/SocialMediaLink.jsx
+++ src/components/SocialMediaLink.jsx
@@ -6,9 +6,9 @@ import linkedInIcon from "../images/linkedInIcon.ico"
 function SocialMediaLink () {
     return (<span>
 
-<a href="https://www.linkedin.com/in/ezekiel-everest/" className=" iconListItem liLink ml-5 mr-3" src={linkedInIcon}>LinkedIn</a>
-<a href="https://github.com/ezekielenyart" className="iconListItem ghLink mx-3" src={githubIcon}>Github</a>
-<a href="https://www.facebook.com/ezekiel.enyart" className="iconListItem fbLink mx-3" src={fbIcon}>Facebook</a>
+<a href="https://www.linkedin.com/in/ezekiel-everest/" target="_blank" className=" iconListItem liLink ml-5 mr-3" src={linkedInIcon}>LinkedIn</a>
+<a href="https://github.com/ezekieleverest" target="_blank" className="iconListItem ghLink mx-3" src={githubIcon}>Github</a>
+<a href="https://www.facebook.com/ezekiel.enyart" target="_blank" className="iconListItem fbLink mx-3" src={fbIcon}>Facebook</a>
 <a className="iconListItem email mx-3">zekejeverest@gmail.com</a>
 
 </span>
